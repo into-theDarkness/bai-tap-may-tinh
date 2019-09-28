f4bbe65b7ab4970915795b1ec0a90018aea70d6a
into-theDarkness <zzpeterpan0510@gmail.com>
2019-09-28 16:40:37 +0700

    calculator.file

--- calculator.js
+++ calculator.js
@@ -1,8 +1,31 @@
 function phepcong() {
-    let nhap1 = document.getElementById("nhap1").value;
-    let nhap2 =document.getElementById("nhap2").value;
+    let nhap1 = Number(document.getElementById("nhap1").value);
+    let nhap2 =Number(document.getElementById("nhap2").value);
     let result = 0;
-    result = nhap1 + nhap2
+    result = nhap1 + nhap2;
+    document.getElementById("play").innerHTML = result;
 
     
+}
+function pheptru() {
+    let nhap1 = Number(document.getElementById("nhap1").value);
+    let nhap2 =Number(document.getElementById("nhap2").value);
+    result = nhap1 - nhap2;
+    document.getElementById("play").innerHTML = result;
+
+}
+function phepnhan() {
+    let nhap1 = Number(document.getElementById("nhap1").value);
+    let nhap2 =Number(document.getElementById("nhap2").value);
+    result = nhap1 * nhap2;
+
+    document.getElementById("play").innerHTML = result;
+}
+function phepchia() {
+    let nhap1 = Number(document.getElementById("nhap1").value);
+    let nhap2 =Number(document.getElementById("nhap2").value);
+    let result = 0;
+    result = nhap1 / nhap2;
+    document.getElementById("play").innerHTML = result;
+
 }
