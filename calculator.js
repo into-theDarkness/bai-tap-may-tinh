8de35cc48ab3e76cb4d8ec4bf1fa2f503f980c29
into-theDarkness <zzpeterpan0510@gmail.com>
2019-09-28 11:53:15 +0700

    Calculator.file

---
+++ calculator.js
@@ -0,0 +1,8 @@
+function phepcong() {
+    let nhap1 = document.getElementById("nhap1").value;
+    let nhap2 =document.getElementById("nhap2").value;
+    let result = 0;
+    result = nhap1 + nhap2
+
+    
+}
